f77004db131f4550751c26e420bda63777ae2e12
Eth3ri0n <eth3rias@gmail.com>
2024-07-10 21:46:46 +0200

    chore: Update default font family to include 'Poppins'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ["Poppins", ...defaultTheme.fontFamily.sans] },
+    },
   },
   plugins: [],
-}
-
+};
